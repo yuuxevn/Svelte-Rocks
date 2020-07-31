8feb413875aa68b9d348acf63267b1ef6a283351
kridopambudi <kridopebe@gmail.com>
2020-08-01 06:17:14 +0700

    :book: Svelte

---
+++ my-svelte-project/src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	// import
+	import EmojiDisplay from './EmojiDisplay.svelte'
+	import EmojiDes from './EmojiDes.svelte'
+
+	let currentEmoji = 'A';
+	const emojis = [ 'B', 'C', 'D', 'E' ];
+	
+	function randomizeEmoji() {
+		return emojis[ Math.floor( Math.random() * emojis.length ) ]
+	}
+	function randomButton() {
+		currentEmoji = randomizeEmoji();
+	}
+
+</script>
+	
+<style>
+	div {
+		margin: 5em;
+	}
+	button {
+		background-color: #8bd3dd;
+		padding: 0.75em;
+		border-radius: 0.25em;
+		border: 2px solid #000;
+		box-shadow: 0.4rem 0.4rem #222;
+	}
+	button:hover {
+		box-shadow: 0.25rem 0.25rem #222;
+		transition: all 0.4 ease 0s;
+		background-color: #8bd;
+	}
+</style>
+
+<h1>Randomize Emoji</h1>
+<EmojiDisplay { currentEmoji } />
+<EmojiDes />
+<button on:click={ randomButton } >Button</button>
+
---
+++ my-svelte-project/src/EmojiDes.svelte
@@ -0,0 +1,24 @@
+<script>
+    let description = '';
+    $: character = description.length;
+    $: words = description.split(' ').length;
+
+    $: if( description.length >= 10 ){
+        console.log('-----');
+        console.log('Description : ${ description }')
+    }
+</script>
+
+<style>
+    div {
+        margin-bottom: 1em;
+        margin-top: 1em;
+    }
+</style>
+
+<div>
+    <p>{ description }</p>
+    <textarea bind:value={ description } cols="30" rows="10"></textarea>
+    <p>Characters count : { character }</p>
+    <p>word count : { words }</p>
+</div>
